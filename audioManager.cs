45b14dfc14f5bae2dc83612725f9fa695ea1f0f9
Wikimaru <71822721+Wikimaru@users.noreply.github.com>
2020-09-24 17:36:58 +0700

    Add files via upload

---
+++ audioManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioManager : MonoBehaviour
+{
+    public static audioManager instance;
+
+    public AudioSource levelMusic, gameOverMusic, winMusic;
+
+    public AudioSource[] sfx; 
+
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayGameOver()
+    {
+        levelMusic.Stop();
+
+        gameOverMusic.Play();
+    }
+    public void PlayLevelWin()
+    {
+        levelMusic.Stop();
+
+        winMusic.Play();
+    }
+
+    public void playSFX(int sfxToPlay)
+    {
+        sfx[sfxToPlay].Stop();
+        sfx[sfxToPlay].Play();
+    }
+}
